41c9e90bc64d70b86ed1df5b4c9921123c33dfce
benjaminKeck <benjamin_keckes@web.de>
2014-06-15 22:58:57 +0200

    Ein richtiges Feld mit Status wird erzeugt

--- src/War.hs
+++ src/War.hs
@@ -3,10 +3,15 @@ module War where
 import Datatypes
 import Data.Char (ord)
 
---Diese Funktion erstellt ein Feld
-makeField::Char -> Integer -> [Coord] -> [Coord]
-makeField 'A' i l = makeLine 'A' i l
-makeField a i l = makeField (getNextChar a) i $ makeLine a i l
+--Diese Funktion erstellt ein Feld mit Status
+makeField::Char -> Integer -> Field -> Field
+makeField 'A' i l = makeLine' 'A' i l
+makeField a i l = makeField (getNextChar a) i $ makeLine' a i l
+
+--Diese Funktion erstellt eine Coordinatenliste
+makeCoordinates::Char -> Integer -> [Coord] -> [Coord]
+makeCoordinates 'A' i l = makeLine 'A' i l
+makeCoordinates a i l = makeCoordinates (getNextChar a) i $ makeLine a i l
 
 --Diese Funktion erstellt eine Line von Coordinaten
 makeLine::Char -> Integer -> [Coord] -> [Coord]
@@ -19,8 +24,10 @@ makeCoord a b = (a,b)
 getNextChar::Char -> Char
 getNextChar a = toEnum((ord a)-1)::Char
 
+makeLine'::Char -> Integer -> Field -> Field
+makeLine' a 0 c = c
+makeLine' a b c = makeLine' a (b-1) ((makeFieldValue (makeCoord a b)):c)
 
+makeFieldValue :: Coord -> (Coord,Status)
+makeFieldValue a = (a, None)
 
---if b > 0 
-					--then makeLine a (b-1) ([a,b] ++ [x])
-					--else [x]
