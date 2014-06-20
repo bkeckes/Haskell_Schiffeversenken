82dc5f56c75f65217fbec083a90ebdf8c75f1cb1
GastonLeGaffeur <nassar@hm.edu>
2014-06-20 17:51:37 +0200

    Userinterfaces Compiliert

--- src/UserInterface.hs
+++ src/UserInterface.hs
@@ -31,9 +31,9 @@ printNumbers = "    1   2   3   4   5   6   7   8   9   10\n\n"
 printShoot :: Maybe Status -> String
 printShoot x
     |  x ==Nothing = "    "
-    |  x ==Fail = "   O"
-	|  x ==Hit = "   X"
-	|  x ==Destroyed = "   #"
+    |  x ==Just Fail = "   O"
+	|  x ==Just Hit = "   X"
+	|  x ==Just Destroyed = "   #"
   
 
 -- Druckt die Reihen
