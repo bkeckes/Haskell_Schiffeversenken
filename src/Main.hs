ef077792a7209cb2d6868412c0021c884ae933a2
MichaelaLutz <lutz6@hm.edu>
2014-07-05 12:12:14 +0200

    Server, Client auf aktuellsten Stand gesetzt. Main Logic nichts verändert.

--- src/Main.hs
+++ src/Main.hs
@@ -10,10 +10,15 @@ import Logic
 
 import qualified Data.Map as M
 
-ships = generateMyShips
+ships =generateMyShips -- [[((fromIntegral 1::Int,fromIntegral 2::Int),Hit),((fromIntegral 1::Int,fromIntegral 3::Int),Hit)]]--generateMyShips
 myfield = initializeField ships
-gameStatus = Game { myField = myfield, enemyField=M.empty, myShips=ships }      --gameLoop gamestatus
+gameStatus = Game { myField = myfield, enemyField=M.empty, myShips=ships, turn=Enemy }	--gameLoop gamestatus
 myTurn=False
+pseudoCoords=(1,7)
+fluchtwert=(2,2)
+pseudoStatus=Hit--Destroyed
+anfang=(1,2)
+ende=(1,5)
            
 --enemyField = M.fromList[((1,1),Fail),((1,5),Hit),((1,10),Destroyed),((9,3),Hit)]
 
@@ -21,28 +26,81 @@ main :: IO ()
 main = do
     putStrLn "Willkommen bei Hit the Ships!"
     -- Client.client  = erfraegt Ip-Addresse und stellt Verbindung mit Server her
-    Client.client
-   -- gameLoop gameStatus
-    --Feld Spieler 1 initialisieren
-    
-    --Feld Spieler 2 initialisieren
+    --Client.client
+    gameLoop gameStatus
+ 
 
 gameLoop :: Game -> IO ()
 gameLoop gameStatus = do
-    print(gameStatus)
-
-                -- Koord einlesen vom Client
-                -- Client.send
-                -- Schuss uebertragen an Server und dann weiterleiten an Logic
-                -- Server.receiveCoord
-                -- Antwort von Logic an Server und dann an den Client weiterleiten
-                
-                -- let myGS = gameStatus { myField = ...
-                              -- ,  
-                                                          -- }
-                -- printFields gameStatus'
-                -- gameLoop gameStatus' 
+    if ((turn gameStatus)==Again  )
+        then putStrLn("Feld bereits gespielt, bitte nochmal versuchen")
+        else putStrLn("Feld noch nicht gespielt")
     
+    printMyField $ myField newGameStatus
+    printMyField $ enemyField newGameStatus
+
+    if isHit pseudoCoords (myShips gameStatus)
+		then putStrLn("hit")
+		else putStrLn("fail")
+   --gameLoop newGameStatus	
+	where   newGameStatus= if ((turn gameStatus)==Me ||(turn gameStatus)==Again  )
+                                then myturn gameStatus
+                                else notmyturn gameStatus coords
+                                  where   coords= if ((turn gameStatus)==Enemy  )
+	                                                then pseudoCoords
+	                                                else fluchtwert
+    
+    
+myturn :: Game->Game
+myturn gameStatus=if (coordIsPlayed pseudoC (enemyField gameStatus))
+					  then gameStatus {turn=Again}
+					  else if pseudoS==Destroyed
+                              then Game { myField =  (myField gameStatus), enemyField=(insertStatuus (anfangK,endeK) Destroyed $enemyField gameStatus), myShips=  (myShips gameStatus),turn=Enemy}
+							  else Game { myField =  (myField gameStatus), enemyField=(insertStatus pseudoS (enemyField gameStatus) pseudoC), myShips= (myShips gameStatus), turn=Enemy}
+					          	where    
+								pseudoS=pseudoStatus  --Status der empfangen wurde
+								pseudoC = pseudoCoords  --Koordinaten die eingelesen wurden
+								anfangK = if pseudoS==Destroyed
+											then anfang --aus Netzwerk
+											else fluchtwert
+								endeK = if pseudoS==Destroyed
+											then ende --aus Netzwerk
+											else fluchtwert
+								h=if coordIsPlayed pseudoC (enemyField gameStatus)
+									then print("Koordinate wurde schon gespielt, bitte nochmal")
+									else print("yees")
+									
+									
+notmyturn :: Game->Coord->Game
+notmyturn gameStatus coords=if(isHit coords (myShips gameStatus))
+                                then if isAShipDestroyed newShips 
+                                         then Game{myField =  (insertStatuus  destroyedCoords Destroyed (myField gameStatus)), enemyField=(enemyField gameStatus), myShips=  destroyedShip,turn=Me}
+                                         else Game{myField =  (insertStatus Hit (myField gameStatus) coords), enemyField=(enemyField gameStatus), myShips=  (myShips gameStatus),turn=Me}
+                                else Game{myField =  (insertStatus Fail (myField gameStatus) coords), enemyField=(enemyField gameStatus), myShips=  (myShips gameStatus),turn=Me}
+							   where 
+							   newShips = shootField coords (myShips gameStatus)
+							   destroyedCoords = (getCoordsFromDestroyed newShips)
+							   destroyedShip = (setShipToDestroyed newShips)
+
+    --if myTurn
+      --  then --Koords einlesen
+             --In ship array enthalten-> coordIsPlayed von Logic.hs
+			 --Senden
+			 --Status empfangen
+        --    newEnemyField = if pseudoStatus==Destroyed
+          --      then    --Anfang/Eckpunkt empfangen
+            --            insertStatuus anfang ende Destroyed $enemyField gameStatus
+                        --printMyField $ myField gameStatus
+                        --printMyField newEnemyField
+                       -- newGameStatus = Game { myField = $ myField gameStatus, enemyField=newEnemyField, myShips= $ ships gameStatus}
+                -- gameLoop newGameStatus
+              --  else	
+				--	insertStatus pseudoCoords (enemyField gameStatus) pseudoCoords
+        --     printMyField $ myField gameStatus
+          --   printMyField newEnemyField
+           --  newGameStatus = Game { myField = $ myField gameStatus, enemyField=newEnemyField, myShips= $ ships gameStatus}
+         --else
+          --   printMyField $ myField gameStatus
   --  let enemyField4=insertStatus Hit enemyField4 (2,7) 
   --   if M.lookup (2,8) enemyField3 ==  Just Hit
   --             then putStrLn "yes"
@@ -56,13 +114,18 @@ gameLoop gameStatus = do
         -- Initialisierung
         -- gameLoop myField enemyField sock
 
+        
+
+                
+
+
 
 
 -- Client.printCoord = Koordinaten an GUI senden (vom Server empfangen an Client gesendet)
 
 -- Client.printCoordDestroyed = Koordinaten an GUI senden 
 
--- Client.printStatus = Status an GUI uebertragen 
+-- Client.printStatus = Status an GUI Ÿbertragen 
 
 
                
