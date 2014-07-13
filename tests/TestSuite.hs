9a1f97b96b82dbb0f26a8fdef10110848cb7369b
benjaminKeck <benjamin_keckes@web.de>
2014-07-13 15:46:06 +0200

    Testfaelle ausgebessert

--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -19,11 +19,15 @@ tests :: [Test.Framework.Test]
 tests =
   [  testGroup "HUnit Tests"
     [ testCase "Status einfuegen" test_insertStatus,
-	  --Bennis Tests
-	  testCase "Erkennen der zerstoerten Schiffe" test_isOneShipDestroyed,
-	  testCase "Richtige Koordinaten vom zerstoerten Schiff?" test_getCoordsFromDestroyed,
-	  testCase "Zerstoertes Schiff soll als Destroyed markiert werden" test_setShipToDestroyed,
-	  testCase "Schiffe ueberlagern sich nicht" test_generateMyShips
+	  testCase "Statuus" test_insertStatuus,
+	  testCase "isHit" test_isHit,
+	  testCase "shootField" test_shootField,
+	  testCase "coordIsPlayed" test_coordIsPlayed,
+       --Bennis Tests
+       testCase "Erkennen der zerstoerten Schiffe" test_isOneShipDestroyed,
+       testCase "Richtige Koordinaten vom zerstoerten Schiff?" test_getCoordsFromDestroyed,
+       testCase "Zerstoertes Schiff soll als Destroyed markiert werden" test_setShipToDestroyed,
+       testCase "Schiffe ueberlagern sich nicht" test_generateMyShips
     ]
   ]
 
@@ -44,47 +48,47 @@ test_insertStatus =
 -- | Insert Ship State wird mitgetstet
 test_insertStatuus::Assertion
 test_insertStatuus=
-	let 
-			tstate=Hit
-			tcoords=((1,2)(1,5))
-			tenemyField=M.fromList[((fromIntegral 1::Int,fromIntegral 1::Int),PartShip)]
-	in assertBool "States inserted?"
-			$ ((insertStatus tcoords tstate tenenmyField) M.! (1,2) ==tstate) &&
-				((insertStatus tcoords tstate tenenmyField) M.! (1,3) ==tstate) &&
-				((insertStatus tcoords tstate tenenmyField) M.! (1,4) ==tstate) &&
-				((insertStatus tcoords tstate tenenmyField) M.! (1,5) ==tstate)
--- | isHitInShip wird mitgetestet	
+     let 
+               tstate=Hit
+               tcoords=((1,2),(1,5))
+               tenemyField=M.fromList[((fromIntegral 1::Int,fromIntegral 1::Int),PartShip)]
+     in assertBool "States inserted?"
+               $ ((insertStatuus tcoords tstate tenemyField) M.! (1,2) ==tstate) &&
+                    ((insertStatuus tcoords tstate tenemyField) M.! (1,3) ==tstate) &&
+                    ((insertStatuus tcoords tstate tenemyField) M.! (1,4) ==tstate) &&
+                    ((insertStatuus tcoords tstate tenemyField) M.! (1,5) ==tstate)
+-- | isHitInShip wird mitgetestet     
 test_isHit::Assertion
-test:isHit = 
-	let tcoord=(1,1)
-		tcoord1=(3,3)
-		tships=[[((fromIntegral 3::Int,fromIntegral 3::Int),PartShip),((fromIntegral 4::Int,fromIntegral 3::Int),PartShip),((fromIntegral 5::Int,fromIntegral 3::Int),PartShip)]]
-	in assertBool "Ist die Koordinate in ships enthalten?"
-			$ ((isHit tcoord tships)==False)&&
-			  (isHit tcoord1 tships)
+test_isHit = 
+     let tcoord = (1,1)
+         tcoord1 = (3,3)
+         tships=[[((fromIntegral 3::Int,fromIntegral 3::Int),PartShip),((fromIntegral 4::Int,fromIntegral 3::Int),PartShip),((fromIntegral 5::Int,fromIntegral 3::Int),PartShip)]]
+     in assertBool "Ist die Koordinate in ships enthalten?"
+               $ ((isHit tcoord tships)==False)&&
+                 (isHit tcoord1 tships)
 
 -- | Testet shootShips mit 
 test_shootField::Assertion --Coord->MyShips->MyShips
 test_shootField =
-	let 
-			tcoord=(4,3)
-			tcoord2=(4,4)
-			tships=[[((fromIntegral 3::Int,fromIntegral 3::Int),PartShip),((fromIntegral 4::Int,fromIntegral 3::Int),PartShip),((fromIntegral 5::Int,fromIntegral 3::Int),PartShip)]]
-			tnewships= $ shootField tcoord tships
-	in assertBool "Ship shooted?"
-		$ (isHit tcoord tnewships)&&
-		  ((isHit tcoord2 tnewships)==False)
-		
+     let 
+               tcoord=(4,3)
+               tcoord2=(4,4)
+               tships=[[((fromIntegral 3::Int,fromIntegral 3::Int),PartShip),((fromIntegral 4::Int,fromIntegral 3::Int),PartShip),((fromIntegral 5::Int,fromIntegral 3::Int),PartShip)]]
+               tnewships= shootField tcoord tships
+     in assertBool "Ship shooted?"
+          $ (isHit tcoord tnewships)&&
+            ((isHit tcoord2 tnewships)==False)
+          
 test_coordIsPlayed::Assertion
 test_coordIsPlayed=
-	let 
-			tcoord=(1,1)
-			tcoord2(1,3)
-			tenemyField=M.fromList[((fromIntegral 1::Int,fromIntegral 1::Int),Hit)]
-	in assertBool "Wurde die Koordinate schon gespielt?"
-		$(coordIsPlayed tcoord tenemyField)&&
-		 ((coordIsPlayed tcoord2 tenemyField)==False)
-		
+     let 
+               tcoord=(1,1)
+               tcoord2 = (1,3)
+               tenemyField=M.fromList[((fromIntegral 1::Int,fromIntegral 1::Int),Hit)]
+     in assertBool "Wurde die Koordinate schon gespielt?"
+          $(coordIsPlayed tcoord tenemyField)&&
+           ((coordIsPlayed tcoord2 tenemyField)==False)
+          
 --------------------------------------------------
 -- Bennis Tests
 ---------------------------------------------------
@@ -103,31 +107,31 @@ test_getCoordsFromDestroyed =
         assertBool "Sind das die Koordinaten des zerstoerten Schiffes?"
         $((getCoordsFromDestroyed getS == ((2,1),(2,3)) ) && 
           ( getCoordsFromDestroyed [getTS1, getTS5, getTS6] == ((2,4),(2,8)) ) &&
-		  ( getCoordsFromDestroyed [getTS1, getTS5, getTS4] == ((0,0),(0,0)) )
-		 )
+            ( getCoordsFromDestroyed [getTS1, getTS5, getTS4] == ((0,0),(0,0)) )
+           )
 
-		 
+           
 test_setShipToDestroyed :: Assertion
 test_setShipToDestroyed = assertBool "Hat sich Status geaendert?"
         $((setShipToDestroyed [getTS1, getTS5, getTS2] == [getTS1, getTS5, [((2,1),Destroyed), ((2,2),Destroyed), ((2,3), Destroyed)]] ) &&
           ( setShipToDestroyed [getTS1, getTS5, getTS3] == [getTS1, getTS5, getTS3] ) &&
-		  ( setShipToDestroyed [getTS1, getTS5, getTS4] == [getTS1, getTS5, getTS4] )
-		 )
+            ( setShipToDestroyed [getTS1, getTS5, getTS4] == [getTS1, getTS5, getTS4] )
+           )
 
 test_generateMyShips :: Assertion
-test_generateMyShips = assertBool "Kommt eine Koordinate oefter vor?"		 
-		$(( kommtDoppelt (makeOneList [getTS1, getTS5, getTS2] []) [] == False ) &&
+test_generateMyShips = assertBool "Kommt eine Koordinate oefter vor?"           
+          $(( kommtDoppelt (makeOneList [getTS1, getTS5, getTS2] []) [] == False ) &&
           ( kommtDoppelt (makeOneList [getTS1, getTS5, getTS4] []) [] == True ) &&
-		  ( kommtDoppelt (makeOneList generateMyShips []) [] == False ) 
-		)
-		 
--- Hilfsfunktion für das Testen ob eine Koordinate öfter verwendet wird. Also ob sich Schiffe überlagern		 
+            ( kommtDoppelt (makeOneList generateMyShips []) [] == False ) 
+          )
+           
+-- Hilfsfunktion für das Testen ob eine Koordinate öfter verwendet wird. Also ob sich Schiffe überlagern           
 kommtDoppelt :: [(Coord,Status)] -> [(Coord,Status)] -> Bool
 kommtDoppelt [] _ = False
 kommtDoppelt (x:xs) schonGehabt = if(x `elem` schonGehabt)
                                    then True
                                    else (kommtDoppelt xs (x:schonGehabt))
-		 
+           
 getTS1::Ship
 getTS1 = [((9,1),Fail), ((9,2),Fail), ((9,3), Fail)]
 
